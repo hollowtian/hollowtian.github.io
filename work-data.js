bc4111e8d2946493cae6fe326eaeb45a8ec3dd55
hollowtian <hollowtian@gmail.com>
2026-09-17 10:50:23 -0400

    Update work-data.js

--- work-data.js
+++ work-data.js
@@ -35,7 +35,7 @@ const WORKS = {
       image: 
         "images/projects/metropolitan-park/01-hero.jpg",
 
-      position: "50% 50%"
+      position: "0% 50%"
 
     },
 
@@ -55,7 +55,7 @@ const WORKS = {
       image: 
          "images/projects/metropolitan-park/01-hero.jpg",
 
-      position: "50% 50%"
+      position: "0% 50%"
 
     },
 
@@ -79,7 +79,7 @@ const WORKS = {
       image: 
          "images/projects/metropolitan-park/01-hero.jpg",
 
-      position: "50% 50%"
+      position: "0% 50%"
 
     },
 
